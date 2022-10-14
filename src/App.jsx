02f0e49aaebe37dfa18168ad15fda90f0f9032f4
JackTubby <jack200034@hotmail.com>
2022-10-14 19:23:22 +0100

    Add resumeType component into app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import ResumeType from './assets/resume-type'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Working!</h1>
+      <ResumeType />
     </div>
   )
 }
