70713bb9a3f89e68ad7fdeaa4599c36dfa1a885c
JackTubby <jack200034@hotmail.com>
2022-10-14 19:22:04 +0100

    Create title & style it

---
+++ src/assets/header/Title.jsx
@@ -0,0 +1,14 @@
+function Title() {
+
+    const titleStyle = {
+        color: '#4798FF',
+        fontWeight: '300',
+        paddingBottom: '16px',
+    }
+
+    return (
+        <h1 style={titleStyle}>Resume Builder</h1>
+    )
+}
+
+export default Title
