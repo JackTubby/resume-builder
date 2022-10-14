650abde1696c5e1a1b4cb6e92d4636dd0e6badfc
JackTubby <jack200034@hotmail.com>
2022-10-14 19:37:27 +0100

    Create data for resume type options

---
+++ src/assets/resume-type/resumeTypeData.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        id: 1,
+        resume: "development"
+    },
+    {
+        id: 2,
+        resume: "resume type 2"
+    },
+    {
+        id: 3,
+        resume: "resume type 3"
+    }
+]
