d9746b1727127f00e848f623bb9562863479e4d0
JackTubby <jack200034@hotmail.com>
2022-10-14 19:40:16 +0100

    Create home file with header and resume type comp

---
+++ src/assets/Home.jsx
@@ -0,0 +1,13 @@
+import Header from './header/Header'
+import ResumeType from './resume-type/ResumeType'
+
+function Home() {
+    return(
+        <main>
+            <Header />
+            <ResumeType />
+        </main>
+    )
+}
+
+export default Home
